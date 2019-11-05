c91f36a04f27ffec533a937a855ebd904ca60618
Christopher Hall <chrishall78@gmail.com>
2019-11-05 10:07:33 -0600

    Added some new entries to the "Work" page.

--- scripts/work.vue.js
+++ scripts/work.vue.js
@@ -26,9 +26,29 @@ var workItem = Vue.component('work-item', {
 var workData = {
 	"portfolio": [
 		{
+			"name": "Pomaika'i Ballrooms",
+			"description": "I worked with Anthology Marketing Group to revamp the website for Pomaika'i Ballrooms, a social event space and catering company. The focus of the rebrand was to showcase the event space and food menus, plus make it easier to plan an event.",
+			"date": "Oct 2019",
+			"liveSiteUrl": "https://www.pomaikaiballrooms.com/",
+			"liveSiteText": "pomaikaiballrooms.com",
+			"thumbImageUrl": "./images/work/Pomaikai-Thumb.jpg",
+			"topImageUrl": "./images/work/Pomaikai-Top.jpg",
+			"tags": ["Front-end dev", "Wordpress integration", "Shopify theme", "WCAG 2.0 AA compliance"]
+		},
+		{
+			"name": "Pualeilani Atrium Shops",
+			"description": "I worked with Anthology Marketing Group to rebrand Pualeilani Atrium Shops' website, a shopping center located within the Hyatt Regency Waikiki Beach Resort &amp; Spa. The site features a dynamic listing of stores, dining, events, plus an interactive directory map.",
+			"date": "Sep 2019",
+			"liveSiteUrl": "https://www.pualeilanishops.com/",
+			"liveSiteText": "pualeilanishops.com",
+			"thumbImageUrl": "./images/work/Pualeilani-Thumb.jpg",
+			"topImageUrl": "./images/work/Pualeilani-Top.jpg",
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+		},
+		{
 			"name": "Zippy's Restaurant",
-			"description": "I worked with the team at Anthology to rebrand the website for Zippy's, an iconic Hawaii restaurant. This was timed with offering new &quot;Order &amp; Pickup&quot; functionality via a 3rd party service.",
-			"date": "05/2019",
+			"description": "I worked with Anthology Marketing Group to rebrand the website for Zippy's, an iconic Hawaii restaurant. This was timed with offering new &quot;Order &amp; Pickup&quot; functionality via a 3rd party service.",
+			"date": "May 2019",
 			"liveSiteUrl": "https://www.zippys.com/",
 			"liveSiteText": "zippys.com",
 			"thumbImageUrl": "./images/work/Zippys-Thumb.jpg",
@@ -37,8 +57,8 @@ var workData = {
 		},
 		{
 			"name": "Servco Corporate",
-			"description": "I worked with the team at Anthology to update the corporate website for Servco. We built a responsive, accessible site to help them highlight their 100 year anniversary.",
-			"date": "02/2019",
+			"description": "I worked with Anthology Marketing Group to update the corporate website for Servco. We built a responsive, accessible site to help them highlight their 100 year anniversary.",
+			"date": "Feb 2019",
 			"liveSiteUrl": "https://www.servco.com/",
 			"liveSiteText": "servco.com",
 			"thumbImageUrl": "./images/work/Servco-Thumb.jpg",
@@ -47,8 +67,8 @@ var workData = {
 		},
 		{
 			"name": "HTA Partner Opportunities",
-			"description": "I worked with the team at Anthology to to build a web application for HTA, so they can manage opportunities to promote Hawaii as a travel/tourism destination. This public portion of the app is to collect info from people interested in the posted opportunities.",
-			"date": "02/2019",
+			"description": "I worked with Anthology Marketing Group to build a web application for HTA, so they can manage opportunities to promote Hawaii as a travel/tourism destination. This public portion of the app is to collect info from people interested in the posted opportunities.",
+			"date": "Feb 2019",
 			"liveSiteUrl": "https://www.hawaiitourismauthority.org//what-we-do/partner-opportunities/",
 			"liveSiteText": "hawaiitourismauthority.org",
 			"thumbImageUrl": "./images/work/HTA-Partner-Opportunities-Thumb.jpg",
@@ -57,8 +77,8 @@ var workData = {
 		},
 		{
 			"name": "Hawaii Tourism Authority",
-			"description": "I worked with the team at Anthology to make the Hawaii Tourism Authority's website attractive, responsive and accessible. It features an extensive research section, and information on Hawaii programs, cultural and natural resources.",
-			"date": "06/2018",
+			"description": "I worked with Anthology Marketing Group to make the Hawaii Tourism Authority's website attractive, responsive and accessible. It features an extensive research section, and information on Hawaii programs, cultural and natural resources.",
+			"date": "Jun 2018",
 			"liveSiteUrl": "https://www.hawaiitourismauthority.org/",
 			"liveSiteText": "hawaiitourismauthority.org",
 			"thumbImageUrl": "./images/work/HTA-Thumb.jpg",
@@ -68,7 +88,7 @@ var workData = {
 		{
 			"name": "Foodland",
 			"description": "",
-			"date": "09/2017",
+			"date": "Sep 2017",
 			"liveSiteUrl": "https://www.foodland.com/",
 			"liveSiteText": "foodland.com",
 			"thumbImageUrl": "./images/work/Foodland-Thumb.jpg",
@@ -78,7 +98,7 @@ var workData = {
 		{
 			"name": "Drive Electric Hawaii",
 			"description": "",
-			"date": "08/2017",
+			"date": "Aug 2017",
 			"liveSiteUrl": "https://www.driveelectrichi.com/",
 			"liveSiteText": "driveelectrichi.com",
 			"thumbImageUrl": "./images/work/Drive-Electric-Hawaii-Thumb.jpg",
@@ -87,8 +107,8 @@ var workData = {
 		},
 		{
 			"name": "Aloha Pacific FCU",
-			"description": "",
-			"date": "09/2016",
+			"description": "I worked with Anthology Marketing Group to update the website for Aloha Pacific FCU, making it mobile friendly and accessible.",
+			"date": "Sep 2016",
 			"liveSiteUrl": "https://www.alohapacific.com/",
 			"liveSiteText": "alohapacific.com",
 			"thumbImageUrl": "./images/work/Aloha-Pacific-Thumb.jpg",
@@ -97,8 +117,8 @@ var workData = {
 		},
 		{
 			"name": "Anthology Marketing Group",
-			"description": "",
-			"date": "08/2015",
+			"description": "I helped build a new version of the Anthology Marketing Group website which made it easier for the company to showcase its clients, work, and staff members.",
+			"date": "Aug 2015",
 			"liveSiteUrl": "https://www.anthologygroup.com/",
 			"liveSiteText": "anthologygroup.com",
 			"thumbImageUrl": "./images/work/Anthology-Group-Thumb.jpg",
