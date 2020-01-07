81904852872330c1c7b7539d59d37f8e85496ee7
Christopher Hall <chrishall78@gmail.com>
2020-01-07 07:21:29 -0600

    Add additional work items, remove coding tips and codepen links.

--- scripts/work.vue.js
+++ scripts/work.vue.js
@@ -26,6 +26,17 @@ var workItem = Vue.component('work-item', {
 var workData = {
 	"portfolio": [
 		{
+			"name": "Kalaeloa Partners",
+			"description": "I worked with Anthology Marketing Group to build a website for Kalaeloa Partners, an energy company located on Oahu, Hawaii. In addition to the front end build, I integrated the site into Wordpress usng custom fields and custom post types.",
+			"date": "Nov 2019",
+			"liveSiteUrl": "#",
+			"liveSiteText": "Not Launched Yet",
+			"thumbImageUrl": "./images/work/Kalaeloa-Thumb.jpg",
+			"topImageUrl": "./images/work/Kalaeloa-Top.jpg",
+			"tags": ["Front-end dev", "Wordpress integration", "WCAG 2.0 AA compliance"],
+			"published": 0
+		},
+		{
 			"name": "Pomaika'i Ballrooms",
 			"description": "I worked with Anthology Marketing Group to revamp the website for Pomaika'i Ballrooms, a social event space and catering company. The focus of the rebrand was to showcase the event space and food menus, plus make it easier to plan an event.",
 			"date": "Oct 2019",
@@ -33,7 +44,8 @@ var workData = {
 			"liveSiteText": "pomaikaiballrooms.com",
 			"thumbImageUrl": "./images/work/Pomaikai-Thumb.jpg",
 			"topImageUrl": "./images/work/Pomaikai-Top.jpg",
-			"tags": ["Front-end dev", "Wordpress integration", "Shopify theme", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "Wordpress integration", "Shopify theme", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Pualeilani Atrium Shops",
@@ -43,7 +55,8 @@ var workData = {
 			"liveSiteText": "pualeilanishops.com",
 			"thumbImageUrl": "./images/work/Pualeilani-Thumb.jpg",
 			"topImageUrl": "./images/work/Pualeilani-Top.jpg",
-			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Zippy's Restaurant",
@@ -53,7 +66,8 @@ var workData = {
 			"liveSiteText": "zippys.com",
 			"thumbImageUrl": "./images/work/Zippys-Thumb.jpg",
 			"topImageUrl": "./images/work/Zippys-Top.jpg",
-			"tags": ["Front-end dev", "Wordpress integration", "Shopify theme", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "Wordpress integration", "Shopify theme", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Servco Corporate",
@@ -63,7 +77,8 @@ var workData = {
 			"liveSiteText": "servco.com",
 			"thumbImageUrl": "./images/work/Servco-Thumb.jpg",
 			"topImageUrl": "./images/work/Servco-Top.jpg",
-			"tags": ["Front-end dev", "Wordpress integration", "Locations map", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "Wordpress integration", "Locations map", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "HTA Partner Opportunities",
@@ -73,7 +88,30 @@ var workData = {
 			"liveSiteText": "hawaiitourismauthority.org",
 			"thumbImageUrl": "./images/work/HTA-Partner-Opportunities-Thumb.jpg",
 			"topImageUrl": "./images/work/HTA-Partner-Opportunities-Top.jpg",
-			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 1
+		},
+		{
+			"name": "Kobayashi Group",
+			"description": "I worked with Anthology Marketing Group to rebrand the Kobayashi Group website. The new site showcases the real estate properties that they have developed, provides a section for news posts, and provides more information about the company and executive team.",
+			"date": "Nov 2018",
+			"liveSiteUrl": "https://www.kobayashi-group.com/",
+			"liveSiteText": "Not Launched Yet",
+			"thumbImageUrl": "./images/work/Kobayashi-Thumb.jpg",
+			"topImageUrl": "./images/work/Kobayashi-Top.jpg",
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 0
+		},
+		{
+			"name": "Halekulani",
+			"description": "I worked with Anthology Marketing Group on implementing a light redesign of the Halekulani website, featuring a new header/footer, home page, and cultural calendar.",
+			"date": "Sep 2018",
+			"liveSiteUrl": "https://www.halekulani.com/",
+			"liveSiteText": "halekulani.com",
+			"thumbImageUrl": "./images/work/Halekulani-Thumb.jpg",
+			"topImageUrl": "./images/work/Halekulani-Top.jpg",
+			"tags": ["Front-end dev", "Drupal integration", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Hawaii Tourism Authority",
@@ -83,37 +121,41 @@ var workData = {
 			"liveSiteText": "hawaiitourismauthority.org",
 			"thumbImageUrl": "./images/work/HTA-Thumb.jpg",
 			"topImageUrl": "./images/work/HTA-Top.jpg",
-			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Foodland",
-			"description": "",
+			"description": "I worked with Anthology Marketing Group on this rebrand of the Foodland website, and assisted with the integration into their Drupal content management system. ",
 			"date": "Sep 2017",
 			"liveSiteUrl": "https://www.foodland.com/",
 			"liveSiteText": "foodland.com",
 			"thumbImageUrl": "./images/work/Foodland-Thumb.jpg",
 			"topImageUrl": "./images/work/Foodland-Top.jpg",
-			"tags": ["Front-end dev", "Drupal integration"]
+			"tags": ["Front-end dev", "Drupal integration"],
+			"published": 1
 		},
 		{
 			"name": "Drive Electric Hawaii",
-			"description": "",
+			"description": "I worked with Anthology Marketing Group on this website promoting the use of electric vehicles in Hawaii.",
 			"date": "Aug 2017",
 			"liveSiteUrl": "https://www.driveelectrichi.com/",
 			"liveSiteText": "driveelectrichi.com",
 			"thumbImageUrl": "./images/work/Drive-Electric-Hawaii-Thumb.jpg",
 			"topImageUrl": "./images/work/Drive-Electric-Hawaii-Top.jpg",
-			"tags": ["Front-end dev"]
+			"tags": ["Front-end dev"],
+			"published": 1
 		},
 		{
 			"name": "Aloha Pacific FCU",
-			"description": "I worked with Anthology Marketing Group to update the website for Aloha Pacific FCU, making it mobile friendly and accessible.",
+			"description": "I worked with Anthology Marketing Group to update the website for Aloha Pacific FCU, making it mobile friendly and accessible. I also worked on associated sites for credit unions they later acquired.",
 			"date": "Sep 2016",
 			"liveSiteUrl": "https://www.alohapacific.com/",
 			"liveSiteText": "alohapacific.com",
 			"thumbImageUrl": "./images/work/Aloha-Pacific-Thumb.jpg",
 			"topImageUrl": "./images/work/Aloha-Pacific-Top.jpg",
-			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 1
 		},
 		{
 			"name": "Anthology Marketing Group",
@@ -123,7 +165,30 @@ var workData = {
 			"liveSiteText": "anthologygroup.com",
 			"thumbImageUrl": "./images/work/Anthology-Group-Thumb.jpg",
 			"topImageUrl": "./images/work/Anthology-Group-Top.jpg",
-			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"]
+			"tags": ["Front-end dev", "WCAG 2.0 AA compliance"],
+			"published": 1
+		},
+		{
+			"name": "FestPro iOS & Android Apps",
+			"description": "Starting in 2012, I developed a native iOS app for the film festival, and later a native Android version. These apps synced film and schedule data from the web version through an API.",
+			"date": "2012 - 2016",
+			"liveSiteUrl": "#",
+			"liveSiteText": "No Longer Available",
+			"thumbImageUrl": "./images/work/FestPro-App-Thumb.jpg",
+			"topImageUrl": "./images/work/FestPro-App-Top.jpg",
+			"tags": ["Design", "iOS App", "Android App", "Objective-C", "Java"],
+			"published": 0
+		},
+		{
+			"name": "FestPro Web Application",
+			"description": "I built this web app for the Hawaii International Film Festival to manage the film program, schedule, and guest information for their semi-annual events. It also had a user facing version which was designed to match their current website.",
+			"date": "2010 - 2016",
+			"liveSiteUrl": "#",
+			"liveSiteText": "No Longer Available",
+			"thumbImageUrl": "./images/work/FestPro-Web-Thumb.jpg",
+			"topImageUrl": "./images/work/FestPro-Web-Top.jpg",
+			"tags": ["Design", "Front-end dev", "Back-end dev", "PHP", "MySQL", "PDF generation", "API"],
+			"published": 1
 		},
 	],
 }
@@ -132,5 +197,12 @@ if (document.getElementById("workContainer") != null) {
 	var vWorkList = new Vue({
 		el: "#workContainer",
 		data: workData,
+		computed: {
+			publishedSites() {
+				return this.portfolio.filter(site => {
+					return site.published > 0;
+				});
+			}
+		}
 	});
 }
